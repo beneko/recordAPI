70b54dff107848a7733548cb876f34d393e61e33
beneko <behzadnekouei@gmail.com>
2021-08-31 11:55:29 +0200

    read.php is created to read the product form bdd and return response

---
+++ model/read.php
@@ -0,0 +1,46 @@
+<?php
+// Les headers dont nous avons besoin
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+require 'config.php';
+$result= $crud->getDiscList();
+$num = $result->rowCount();
+
+if($num>0) {
+
+    // tableau de produits
+    $discs = array();
+    $discs["records"] = array();
+
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+
+        extract($row);
+
+        $disc = array(
+            "disc_id" => $disc_id,
+            "artist_name" => $artist_name,
+            "disc_title" => $disc_title,
+            "disc_label" => $disc_label,
+            "disc_year" => $disc_year,
+            "disc_genre" => $disc_genre,
+            "disc_picture" => $disc_picture
+        );
+
+        array_push($discs["records"], $disc);
+    }
+    // on envoie la réponse http à 200 OK
+    http_response_code(200);
+
+    // on renvoie la réponse en Json
+    echo json_encode($discs);
+
+}
+else
+{
+    // on renvoie le code 404 Not found
+    http_response_code(404);
+
+    // On averti l'utilisateur
+    echo json_encode(array("message"=> "Aucun produits trouvés."));
+}
